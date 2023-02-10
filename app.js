d399e0b1d05b6ae26f8beb5cad93c9313d5224c2
Aren <arenaguila@gmail.com>
2023-02-09 20:06:27 -0800

    Quick Update

---
+++ app.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var text = document.getElementById("whoDesc");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
